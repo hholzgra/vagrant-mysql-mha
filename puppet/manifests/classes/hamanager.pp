a9aa2409e4add3bd4a81e98751bb0a7fadd28489
Hartmut Holzgraefe <hartmut@php.net>
2011-09-26 18:28:47 +0200

    mha manager depends on mha node

--- puppet/manifests/classes/hamanager.pp
+++ puppet/manifests/classes/hamanager.pp
@@ -3,9 +3,8 @@ class hamanager {
     ensure => present,
   }
 
-
   package { "mha4mysql-manager":
-    require => Package["libdbd-mysql-perl","libconfig-tiny-perl","liblog-dispatch-perl","libparallel-forkmanager-perl"],
+    require => Package["libdbd-mysql-perl","libconfig-tiny-perl","liblog-dispatch-perl","libparallel-forkmanager-perl","mha4mysql-node"],
     ensure => present,
     provider => dpkg,
     source => "/vagrant/puppet/files/packages/mha4mysql-manager_0.52_all.deb",
